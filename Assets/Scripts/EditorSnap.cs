b57da3f18ad5c051f8cf31eee17455e764e00099
Mark Ryder <markmryder@gmail.com>
2020-09-13 21:26:52 -0400

    Created basic folder structure in assets

--- Assets/Scripts/EditorSnap.cs
+++ Assets/Scripts/EditorSnap.cs
@@ -18,9 +18,8 @@ public class EditorSnap : MonoBehaviour
     void Update()
     {
         Vector3 snapPosition;
-        snapPosition.x = Mathf.RoundToInt(transform.position.x / 10f) * 10f;
-        snapPosition.y = Mathf.RoundToInt(transform.position.y / 10f) * 10f;
-        snapPosition.z = Mathf.RoundToInt(transform.position.z / 10f) * 10f;
+        snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         transform.position = new Vector3(snapPosition.x, 0f, snapPosition.z);
     }
